78291638f92778cbed15f639bd5f1ee62628baed
dchardin <donnie@fedoraproject.org>
2017-10-24 10:30:53 -0400

    adding instructions for installing java and netbeans.

--- F26_XFCE_PostInstallScript.sh
+++ F26_XFCE_PostInstallScript.sh
@@ -39,6 +39,26 @@
 # ./VBoxLinuxAdditions.run
 # reboot
  
+# Install Java and Netbeans
+#
+# Download and localinstall jdk-8u151-linux-x64.rpm
+#
+# alternatives --install /usr/bin/java java /usr/java/latest/jre/bin/java 200000
+# alternatives --install /usr/bin/javaws javaws /usr/java/latest/jre/bin/javaws 200000
+# alternatives --install /usr/lib64/mozilla/plugins/libjavaplugin.so libjavaplugin.so.x86_64 /usr/java/latest/jre/lib/amd64/libnpjp2.so 200000
+# alternatives --install /usr/bin/javac javac /usr/java/latest/bin/javac 200000
+# alternatives --install /usr/bin/jar jar /usr/java/latest/bin/jar 200000
+
+# java -version
+# javaws
+# javac -version
+# export JAVA_HOME="/usr/java/latest"
+
+# now go out to netbeans and download the installer script.
+# as root, chmod 777 and run and ensure that you select "customize" in order
+# to install tomcat as well.
+#
+
 
 
 
